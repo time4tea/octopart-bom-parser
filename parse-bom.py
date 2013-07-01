928f06bd08671797252c1fbd10f920adafddd086
James Richardson <james@time4tea.net>
2013-07-01 19:40:23 +0100

    bit more multicurrency stuff -

--- parse-bom.py
+++ parse-bom.py
@@ -1,24 +1,63 @@
 import csv
+import money
 from octopartv3 import Octopart
 from decimal import Decimal
+
 
 def batch_of(data, size=20):
     for i in range(0, len(data), size):
         yield data[i:i + size]
 
 
+class StringMatch:
+
+    def __init__(self, s):
+        self._s = s
+
+    def matches(self, s):
+        return self._s == s
+
+
+class AnyMatch:
+
+    def matches(self,s):
+        return True
+
+
+class InMatch:
+
+    def __init__(self, l):
+        self._l = l
+
+    def matches(self, i):
+        return i in self._l
+
+
+class Price:
+
+    def __init__(self, money, distributor, url):
+        self._money = money
+        self._distributor = distributor
+        self._url = url
+
+    def __str__(self):
+        return "Price: " + str(self._money) + " Dist: " + self._distributor + " URL:" + self._url
+
 import argparse
 
 parser = argparse.ArgumentParser(description="Parse a BOM and ask octopart for the pricing information")
 parser.add_argument("--file", help="file to parse", required=True)
 parser.add_argument("--key", help="Octopart API key", required=True)
-parser.add_argument("--currency", help="Currency to buy items in", default="GBP")
+parser.add_argument("--currency", help="Preferred currency to buy items in", default="GBP")
+parser.add_argument("--dist", help="Limit to a distributor", action="append")
 parser.add_argument("--widgets", type=int, default=1, help="how many widgets you're making")
 args = parser.parse_args()
 
 csv_file = open(args.file)
 
 api = Octopart("http://octopart.com/api/v3/", args.key)
+
+required_distributor = InMatch(args.dist) if args.dist is not None else AnyMatch()
 
 csv_reader = csv.DictReader(csv_file)
 line_items = []
@@ -47,42 +86,43 @@ for batch in batch_of(line_items):
         item = items[result['reference']]
         required_quantity = int(item["Quantity"]) * args.widgets
 
-        best_price = Decimal("Infinity")
-        line_price = Decimal(0)
-        distributor = "-- Not Found -- "
-        product_url = ""
+        valid_prices = []
 
         if result['hits'] > 0:
             for offer in result['items'][0]['offers']:
-
                 if offer['moq'] is None or offer['moq'] < required_quantity:
                     if offer['in_stock_quantity'] > required_quantity:
 
-                        if args.currency not in offer['prices'].keys():
-                            continue
-                        price = None
-                        for price_tuple in offer['prices'][args.currency]:
-                            if price_tuple[0] > required_quantity:
-                                break
-                            price = Decimal(price_tuple[1])
+                        prices = offer['prices']
 
-                            if price < best_price:
-                                best_price = price
-                                line_price = price * required_quantity
-                                product_url = offer["product_url"] if offer["product_url"] is not None else "Dunno"
-                                distributor = offer["seller"]["name"]
+                        for currency in prices.keys():
+                            price = None
+                            for price_tuple in prices[currency]:
+                                if price_tuple[0] > required_quantity:
+                                    break
+                                price = Price(
+                                    money.Money(price_tuple[1], currency),
+                                    offer["seller"]["name"],
+                                    offer["product_url"]
+                                )
+                            if price is not None:
+                                valid_prices.append(price)
 
-        total_cost += line_price
+        for price in valid_prices:
+            print price
 
-        print ",".join([
-            item["Quantity"],
-            item["Part Number"],
-            item["Manufacturer"],
-            distributor,
-            args.currency,
-            str(best_price),
-            str(line_price),
-            product_url
-            ])
+        #line_price = best_price * required_quantity
+        #total_cost += line_price
+
+        #print ",".join([
+        #    item["Quantity"],
+        #    item["Part Number"],
+        #    item["Manufacturer"],
+        #    distributor,
+        #    args.currency,
+        #    str(best_price),
+        #    str(line_price),
+        #    product_url
+        #    ])
 
 
